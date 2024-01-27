24aa6950c39ffc00f9f94f0db82f8883a6313500
Abeera Fatima <120410882+melodyBee@users.noreply.github.com>
2024-01-27 23:32:04 +0500

    Add files via upload

---
+++ Areaofatriangle.py
@@ -0,0 +1,8 @@
+def area() :
+    b=int(input("enter the base of the triangle= ")) 
+    h=int(input("enter the height of the triangle= ")) 
+    A=(b*h)/2
+    print("the area of the triangle is= ", A)
+while True:
+    area() 
+    
